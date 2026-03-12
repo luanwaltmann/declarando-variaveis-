1549ce2c3d9fd6a9ed001cbd850f24abb62a678a
luanwaltmann <luan.waltmann@escola.pr.gov.br>
2026-03-12 15:23:31 +0000

    utilizando-variaveis

---
+++ script.js
@@ -0,0 +1,18 @@
+// var
+
+//var altura = 5;
+//var comprimento =7; 
+
+//area = altura * comprimento;
+//console.log(area);
+
+let forma = 'retângular';
+let altura = 5;
+let comprimento 7;
+
+if ( forma === 'retângulo'){
+area = altura * comprimento;
+} else {
+    area = (altura * comprimento)/2;
+}
+console.log(area)
