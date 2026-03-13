e04c626e0e804693d4a1d1e975e07b2bde530476
luanwaltmann <luan.waltmann@escola.pr.gov.br>
2026-03-13 15:23:02 +0000

    alteracos //

--- script.js
+++ script.js
@@ -6,14 +6,16 @@
 //area = altura * comprimento;
 //console.log(area);
 
-let forma = 'retângular';
-let altura = 5;
-let comprimento = 7;
-let area 
+//let 
 
-if ( forma === 'retângulo'){
-area = altura * comprimento;
-} else {
-    area = (altura * comprimento)/2;
-}
-console.log(area)
+//let forma = 'retângular';
+//let altura = 5;
+//let comprimento = 7;
+//let area 
+
+//if ( forma === 'retângulo'){
+//area = altura * comprimento;
+//} else {
+//    area = (altura * comprimento)/2;
+//}
+//console.log(area)
