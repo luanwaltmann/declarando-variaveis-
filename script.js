2d4f2290031ee52f1302eae286eef301be121346
luanwaltmann <luan.waltmann@escola.pr.gov.br>
2026-03-19 14:58:41 +0000

    .

--- script.js
+++ script.js
@@ -19,3 +19,16 @@
 //    area = (altura * comprimento)/2;
 //}
 //console.log(area)
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma ===  'triangulo'){
+     area = altura * comprimento;
+}else{
+    area =(altura * comprimento) /2;
+}
+console.log (area);
+
